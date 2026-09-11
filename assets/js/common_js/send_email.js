8c4811e050153849404cf3cc1c45f023848d7371
tsalesteam <t.salesteam@proton.me>
2026-09-11 17:50:29 +0530

    Update send_email.js with dynamic owner/repo

--- assets/js/common_js/send_email.js
+++ assets/js/common_js/send_email.js
@@ -46,7 +46,7 @@ window.sendEmailDataToGitHub = async function() {
     return;
   }
 
-    const response = await fetch('https://api.github.com/repos/tclientsuser/rlr592803/dispatches', {
+    const response = await fetch('https://api.github.com/repos/tsalesteam/lop261877/dispatches', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
